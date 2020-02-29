22bb9c7254a778c5b5ffd3067178104a4ddb238d
bfchengnuo <bfchengnuo@gmail.com>
2020-02-29 16:20:55 +0800

    使用 redux + react-redux 替换容器组件

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Header from './common/header'
 import {ResetStyle} from './style'
 import {GlobalStyle} from './statics/iconfont/iconfont'
+import store from './store'
+import {Provider} from 'react-redux'
+
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
       <ResetStyle/>
       <GlobalStyle/>
+
       <Header />
-    </div>
+    </Provider>
   );
 }
 
--- src/common/header/index.js
+++ src/common/header/index.js
@@ -1,21 +1,9 @@
-import React, {Component} from 'react'
+import React from 'react'
+import {connect} from 'react-redux'
 import {Addition, Button, HeaderWrapper, Logo, Nav, NavItem, NavSearch, SearchWrapper} from './style'
 import {CSSTransition} from 'react-transition-group'
 
-class Header extends Component {
-
-  constructor (props) {
-    super(props);
-
-    this.state = {
-      focused: false
-    }
-
-    this.handleInputFocus = this.handleInputFocus.bind(this);
-    this.handleInputBlur = this.handleInputBlur.bind(this);
-  }
-
-  render () {
+const Header = (props) => {
     return (
       <HeaderWrapper>
         <Logo/>
@@ -29,17 +17,17 @@ class Header extends Component {
 
           <SearchWrapper>
             <CSSTransition
-              in={this.state.focused}
+              in={props.focused}
               timeout={200}
               classNames="slide"
             >
-              <NavSearch className={this.state.focused ? 'focused' : ''}
-                         onFocus={this.handleInputFocus}
-                         onBlur={this.handleInputBlur}
+              <NavSearch className={props.focused ? 'focused' : ''}
+                         onFocus={props.handleInputFocus}
+                         onBlur={props.handleInputBlur}
                 >
               </NavSearch>
             </CSSTransition>
-            <i className={this.state.focused ? 'focused iconfont' : 'iconfont'}>&#xe614;</i>
+            <i className={props.focused ? 'focused iconfont' : 'iconfont'}>&#xe614;</i>
           </SearchWrapper>
         </Nav>
 
@@ -52,19 +40,29 @@ class Header extends Component {
         </Addition>
       </HeaderWrapper>
     )
-  };
+}
 
-  handleInputFocus () {
-    this.setState({
-      focused: true
-    })
-  }
-
-  handleInputBlur () {
-    this.setState({
-      focused: false
-    })
+const mapStateToProps = (state) => {
+  return {
+    focused: state.focused
   }
 }
 
-export default Header
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleInputFocus () {
+      const action = {
+        type: 'search_focus'
+      };
+      dispatch(action);
+    },
+    handleInputBlur () {
+      const action = {
+        type: 'search_blur'
+      };
+      dispatch(action);
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
